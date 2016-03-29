1435f2ffc59ab7ef1513174ae22f2c3cdd262e0d
Gabe Fierro <gtfierro225@gmail.com>
2016-03-29 09:20:35 -0700

    migrate server_test to use common package

--- single-node/server_test.go
+++ single-node/server_test.go
@@ -1,16 +1,17 @@
 package main
 
 import (
+	"github.com/gtfierro/cs262-project/common"
 	"gopkg.in/vmihailenco/msgpack.v2"
 	"net"
 	"testing"
 )
 
-var testConfig = Config{
-	Logging: LoggingConfig{UseJSON: false, Level: "debug"},
-	Server:  ServerConfig{Port: 4444, Global: false},
-	Mongo:   MongoConfig{Port: 27017, Host: "0.0.0.0"},
-	Debug:   DebugConfig{Enable: false, ProfileLength: 0},
+var testConfig = common.Config{
+	Logging: common.LoggingConfig{UseJSON: false, Level: "debug"},
+	Server:  common.ServerConfig{Port: 4444, Global: false},
+	Mongo:   common.MongoConfig{Port: 27017, Host: "0.0.0.0"},
+	Debug:   common.DebugConfig{Enable: false, ProfileLength: 0},
 }
 
 func BenchmarkDispatchNoMetadata(b *testing.B) {
@@ -22,7 +23,7 @@ func BenchmarkDispatchNoMetadata(b *testing.B) {
 		b.Fatal(err)
 	}
 	encoder := msgpack.NewEncoder(conn)
-	message := &Message{UUID: "cd47df06-f451-11e5-873b-9b450be7df8d", Value: 0}
+	message := &common.Message{UUID: "cd47df06-f451-11e5-873b-9b450be7df8d", Value: 0}
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		message.Value = i
@@ -43,7 +44,7 @@ func BenchmarkDispatchWithMetadata(b *testing.B) {
 		b.Fatal(err)
 	}
 	encoder := msgpack.NewEncoder(conn)
-	message := &Message{UUID: "cd47df06-f451-11e5-873b-9b450be7df8d", Value: 0, Metadata: map[string]interface{}{"ABC": "123"}}
+	message := &common.Message{UUID: "cd47df06-f451-11e5-873b-9b450be7df8d", Value: 0, Metadata: map[string]interface{}{"ABC": "123"}}
 	b.ReportAllocs()
 	b.ResetTimer()
 	for i := 0; i < b.N; i++ {

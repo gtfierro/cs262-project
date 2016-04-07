02afae6d613a0223f93afe7445eb3c5b3e6a7b8a
Gabe Fierro <gtfierro225@gmail.com>
2016-04-07 06:20:27 -0700

    fixup client goroutine recovery and race conditions. Add some documentation too

--- single-node/client.go
+++ single-node/client.go
@@ -16,9 +16,9 @@ type Client struct {
 	// buffer of messages to send out
 	buffer chan common.Sendable
 	// whether or not client is actively sending messages
-	active  bool
+	active  *AtomicBool
 	timeout *time.Timer
-	stop    chan bool
+	pause   chan bool
 	// send on this channel when we die
 	death   chan<- *Client
 	encoder *msgp.Writer
@@ -29,9 +29,11 @@ func NewClient(query string, conn *net.Conn, death chan<- *Client) *Client {
 		query:   query,
 		conn:    conn,
 		buffer:  make(chan common.Sendable, 1e6), // TODO buffer size?
-		timeout: time.NewTimer(10 * time.Second), // TODO config file
-		active:  true,
-		stop:    make(chan bool),
+		active:  &AtomicBool{0},
+		timeout: time.NewTimer(10 * time.Second),
+		// we don't want to block on sending/receiving the pause
+		// signal because its handled in the same goroutine, so we buffer
+		pause:   make(chan bool, 1),
 		encoder: msgp.NewWriter(*conn),
 		death:   death,
 	}
@@ -39,7 +41,10 @@ func NewClient(query string, conn *net.Conn, death chan<- *Client) *Client {
 
 // queues a message to be sent
 func (c *Client) Send(m common.Sendable) {
-	if !c.active {
+	// if our event loop isn't started, then start it
+	if !c.active.Get() {
+		c.active.Set(true)
+		c.timeout.Reset(10 * time.Second)
 		go c.dosend()
 	}
 	select {
@@ -49,17 +54,35 @@ func (c *Client) Send(m common.Sendable) {
 	}
 }
 
+// This is the client event loop that handles forwarding messages from
+// the broker to the client. It uses channels to communicate. This method
+// is most likely going to run in a goroutine, so we need a way to terminate
+// it and start it when the clients leave/appear again. This is explained below.
 func (c *Client) dosend() {
+	// TODO: put this value in a configuration file
 	for {
 		select {
+		// When clients leave, we don't know that they have failed until we try to forward
+		// them a message and it fails. If we don't do anything about this, then we will
+		// leak goroutines for clients that leave and never come back. So, when the timeout
+		// fires, we pause the client and send a signal on the pause channel
 		case <-c.timeout.C:
-			log.Info("client timeout")
-			c.stop <- true
-		case <-c.stop:
-			c.active = false
+			log.Info("Client timeout!")
+			c.pause <- true
+			// When we receive a message on the pause channel, we mark ourselves as inactive
+			// and pause the timeout timer. We then return so that this goroutine is terminated.
+		case <-c.pause:
+			c.active.Set(false)
 			c.timeout.Stop()
 			return
+			// When we receive a message on the incoming buffer, we we call the Encode() method
+			// on it, and then make sure to Flush() the encoder to actually do the send.
+			// TODO: there is an opportunity here to batch multiple messages into a single send,
+			// by calling Encode() on multiple messages before doing the final flush to send
+			// them over the network. This would be a good thing to look at if it turns out
+			// during benchmarking that we are spending a lot of time on network write syscalls
 		case m := <-c.buffer:
+			// reset the timer to the new default value when we send
 			c.timeout.Reset(10 * time.Second)
 			log.WithFields(log.Fields{
 				"query": c.query, "message": m,
@@ -77,7 +100,9 @@ func (c *Client) dosend() {
 					"error":   err,
 					"message": m,
 				}).Error("Error sending Message to client!")
-				c.active = false
+				// if we error out, stop the timer
+				// to make sure that its goroutine stops, and then queue ourselves
+				// for deletion by sending on the DEATH channel
 				c.timeout.Stop()
 				c.death <- c
 				return

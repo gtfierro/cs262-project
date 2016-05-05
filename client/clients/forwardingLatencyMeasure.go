6abbb8f806a3ed607c652c584517095a04addf31
Gabe Fierro <gtfierro225@gmail.com>
2016-05-04 23:04:13 -0700

    add forwarding latency tests

---
+++ client/clients/forwardingLatencyMeasure.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ccding/go-logging/logging"
+	"github.com/gtfierro/cs262-project/client"
+	"github.com/gtfierro/cs262-project/common"
+	"github.com/montanaflynn/stats"
+	"math/rand"
+	"os"
+	"sync"
+	"time"
+)
+
+var log *logging.Logger
+var r rand.Source
+var wg sync.WaitGroup
+var rate time.Duration
+var waitTime time.Duration
+var numClients int
+var gather chan []float64
+
+func init() {
+	log, _ = logging.WriterLogger("main", logging.DEBUG, logging.BasicFormat, logging.DefaultTimeFormat, os.Stderr, true)
+	r = rand.NewSource(time.Now().UnixNano())
+	rate = time.Duration(100) * time.Millisecond
+	waitTime = 20 * time.Minute
+	numClients = 10
+	gather = make(chan []float64, numClients)
+}
+
+func runPubSubPair(rate time.Duration, subConfig, pubConfig *client.Config) {
+	// 1 client, 1 publisher. Measure tiem between one publishing, one subscribing
+	mdkey := fmt.Sprintf("Room%d", r.Int63())
+	mdval := fmt.Sprintf("%d", r.Int63())
+	query := fmt.Sprintf("%s = '%s'", mdkey, mdval)
+	publisher_seed := fmt.Sprintf("%d", r.Int63())
+	subscriber_seed := fmt.Sprintf("%d", r.Int63())
+
+	latencies := []float64{}
+
+	c, err := client.NewClient(client.UUIDFromName(subscriber_seed), query, subConfig)
+	c.AttachPublishHandler(func(m *common.PublishMessage) {
+		recv_time := time.Now().UnixNano()
+		sent_time := m.Value.(int64)
+		diff := recv_time - sent_time
+		latencies = append(latencies, float64(diff))
+		if len(latencies)%100 == 0 {
+			fmt.Printf("Message: %v. Diff %s\n", len(latencies), time.Duration(diff).String())
+		}
+	})
+	c.Start()
+	time.Sleep(500 * time.Millisecond)
+
+	publisher, err := client.NewPublisher(client.UUIDFromName(publisher_seed), func(pub *client.Publisher) {
+		ticker := time.Tick(rate)
+		pub.AddMetadata(map[string]interface{}{mdkey: mdval})
+		for now := range ticker {
+			if err := pub.Publish(now.UnixNano()); err != nil {
+				log.Error(err)
+			}
+		}
+	}, pubConfig)
+	if err != nil {
+		log.Criticalf("Could not create publisher (%v)", err)
+		os.Exit(1)
+	}
+	publisher.Start()
+
+	c.StopIn(waitTime)
+	<-c.Stop
+	wg.Done()
+	gather <- latencies
+}
+
+func main() {
+	brokerIP := os.Args[1]
+	config := &client.Config{
+		BrokerAddress:      fmt.Sprintf("%s:4444", brokerIP),
+		CoordinatorAddress: "cs262.cal-sdb.org:5505",
+	}
+
+	wg.Add(numClients)
+	for i := 0; i < numClients; i++ {
+		go runPubSubPair(rate, config, config)
+	}
+	wg.Wait()
+	close(gather)
+
+	var latencies [][]float64
+	for data := range gather {
+		latencies = append(latencies, data)
+	}
+
+	quartiles, _ := stats.Quartile(latencies[0])
+	q1 := time.Duration(quartiles.Q1)
+	q2 := time.Duration(quartiles.Q2)
+	q3 := time.Duration(quartiles.Q3)
+	mean, _ := stats.Mean(latencies[0])
+	percent99, _ := stats.Percentile(latencies[0], 99)
+	fmt.Printf("Quartiles. 25%% %s, 50%% %s, 75%% %s\n", q1.String(), q2.String(), q3.String())
+	fmt.Printf("Mean: %s\n", time.Duration(mean).String())
+	fmt.Printf("99 percentile: %s\n", time.Duration(percent99).String())
+	client.ManyColumnCSV(latencies, []string{}, "many_clients.csv")
+}
---
+++ client/clients/forwardingLatencyWithMDChangeNto1.go
@@ -0,0 +1,130 @@
+package main
+
+import (
+	"fmt"
+	"github.com/ccding/go-logging/logging"
+	"github.com/gtfierro/cs262-project/client"
+	"github.com/gtfierro/cs262-project/common"
+	"math/rand"
+	"os"
+	"sync"
+	"time"
+)
+
+// N publishers to a single subscriber
+
+var log *logging.Logger
+var r *rand.Rand
+var wg sync.WaitGroup
+var rate time.Duration
+var waitTime time.Duration
+var numPublishers int
+var numClients int
+var mdkey string
+var query string
+var clientLatencies [][]float64
+var clientLock sync.Mutex
+
+func init() {
+	log, _ = logging.WriterLogger("main", logging.DEBUG, logging.BasicFormat, logging.DefaultTimeFormat, os.Stderr, true)
+	r = rand.New(rand.NewSource(time.Now().UnixNano()))
+	rate = time.Duration(200) * time.Millisecond
+	waitTime = 1 * time.Minute
+	numPublishers = 12
+	numClients = 10
+	mdkey = fmt.Sprintf("Room%d", r.Int63())
+	wg.Add(numClients * numPublishers) // wait for everyone
+	clientLatencies = make([][]float64, numClients)
+}
+
+func runPub(mdval int, rate time.Duration, pubConfig *client.Config) {
+	publisher_seed := fmt.Sprintf("%d", r.Int63())
+	// change mdval to change the metadata
+
+	publisher, err := client.NewPublisher(client.UUIDFromName(publisher_seed), func(pub *client.Publisher) {
+		ticker := time.Tick(rate)
+		val := fmt.Sprintf("%d", mdval)
+		pub.AddMetadata(map[string]interface{}{mdkey: val})
+		for now := range ticker {
+			if err := pub.Publish(now.UnixNano()); err != nil {
+				log.Error(err)
+			}
+		}
+	}, pubConfig)
+	if err != nil {
+		log.Criticalf("Could not create publisher (%v)", err)
+		os.Exit(1)
+	}
+	go func(pub *client.Publisher) {
+		for {
+			time.Sleep(time.Duration(r.Int31n(30)) * time.Second)
+			mdval = (mdval + 1) % 12
+			val := fmt.Sprintf("%d", mdval)
+			log.Infof("chaning metdata to %v", val)
+			pub.AddMetadata(map[string]interface{}{mdkey: val})
+		}
+	}(publisher)
+	publisher.Start()
+
+	publisher.StopIn(waitTime)
+	<-publisher.Stop
+	wg.Done()
+}
+
+func run1sub10pub(idx int, subscribeChunk string, rate time.Duration, pubConfig, subConfig *client.Config) {
+	clientLock.Lock()
+	clientLatencies[idx] = []float64{}
+	clientLock.Unlock()
+
+	switch subscribeChunk {
+	case "1":
+		query = fmt.Sprintf("%s = '0' or %s = '1' or %s = '2' or %s = '3'", mdkey, mdkey, mdkey, mdkey)
+	case "2":
+		query = fmt.Sprintf("%s = '4' or %s = '5' or %s = '6' or %s = '7'", mdkey, mdkey, mdkey, mdkey)
+	case "3":
+		query = fmt.Sprintf("%s = '8' or %s = '9' or %s = '10' or %s = '11'", mdkey, mdkey, mdkey, mdkey)
+	}
+	subscriber_seed := fmt.Sprintf("%d", r.Int63())
+	c, err := client.NewClient(client.UUIDFromName(subscriber_seed), query, subConfig)
+	num := 0
+	c.AttachPublishHandler(func(m *common.PublishMessage) {
+		num += 1
+		recv_time := time.Now().UnixNano()
+		sent_time := m.Value.(int64)
+		diff := recv_time - sent_time
+		clientLock.Lock()
+		clientLatencies[idx] = append(clientLatencies[idx], float64(diff))
+		clientLock.Unlock()
+		if num%100 == 0 {
+			fmt.Printf("Message: %v. Diff %s\n", num, time.Duration(diff).String())
+		}
+	})
+	if err != nil {
+		log.Criticalf("Could not create client (%v)", err)
+		os.Exit(1)
+	}
+	c.Start()
+
+	for i := 0; i < numPublishers; i++ {
+		i := i
+		time.Sleep(time.Duration(r.Int31n(100)) * time.Millisecond)
+		go runPub(i, rate, pubConfig)
+	}
+
+}
+
+func main() {
+	brokerIP := os.Args[1]
+	subscribeChunk := os.Args[2]
+	config := &client.Config{
+		BrokerAddress:      fmt.Sprintf("%s:4444", brokerIP),
+		CoordinatorAddress: "cs262.cal-sdb.org:5505",
+	}
+
+	for i := 0; i < numClients; i++ {
+		idx := i
+		go run1sub10pub(idx, subscribeChunk, rate, config, config)
+	}
+	wg.Wait()
+	client.ManyColumnCSV(clientLatencies, []string{}, "data.csv")
+}
